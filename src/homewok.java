8a05dc0a1d5e157d6acb2c166e078b403880903d
krissloveis <yorkyy@mail.ru>
2024-09-03 20:48:54 +0300

    home work 2

--- src/homewok.java
+++ src/homewok.java
@@ -1,8 +1,37 @@
 public class homewok {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+
+    /*byte countRunCycleForField;
+    countRunCycleForField = 0;
+    byte countRunCycleForField2 = 123;
+    short myShort = 127;
+    int myInt = 127;
+    long myLong = 12000000007L;
+
+    float myFloat = 3.0f;
+    double myDouble = 3.0;
+
+    char myChar = 'a';
+
+    boolean muBoolean = false;*/
+
+//     Арифметические операции с целыми числами
+
+        int a = 16;
+        int b = 5;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b;
+        int remainder = a % b;
+
+        System.out.println("Сложение: " + sum);
+        System.out.println("Вычитание: " + difference);
+        System.out.println("Умножение: " + product);
+        System.out.println("Деление: " + quotient);
+        System.out.println("Остаток от деления: " + remainder);
+
     }
 }
 
