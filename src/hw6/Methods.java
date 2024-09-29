667fd0b76b2008c80c7d96bc6d61dd78b49b52d7
krissloveis <yorkyy@mail.ru>
2024-09-29 22:01:37 +0300

    home work krisss 29

--- src/hw6/Methods.java
+++ src/hw6/Methods.java
@@ -1,25 +1,11 @@
 package hw6;
 
 public class Methods {
-
-    // Метод для вычисления квадрата числа
-
-    public static int square(int number) {
-        return number * number;
-    }
-
-    // Метод для вывода полного имени
-
-    public static void printFullName(String firstName, String lastName) {
-        System.out.println("Full name: " + firstName + " " + lastName);
-    }
-
-    // Метод для нахождения самой короткой и самой длинной строки
-
-    public static String[] cornerValuesArray(String[] strings) {
-
+    // Метод для нахождения самых коротких и длинных строк
+    public static String[] cornerValuesArray(String... strings) {
         String shortest = strings[0];
         String longest = strings[0];
+
         for (String str : strings) {
             if (str.length() < shortest.length()) {
                 shortest = str;
@@ -30,24 +16,50 @@ public class Methods {
         }
         return new String[]{shortest, longest};
     }
-    public static void main(String[] args) {
 
-        // Вызов метода square
+    // Метод для нахождения минимального и максимального значения в массиве int
+    public static int[] cornerValuesArray(int... numbers) {
+        int min = numbers[0];
+        int max = numbers[0];
 
-        System.out.println(square(5)); // 25
-        System.out.println(square(-3)); // 9
-
-        // Вызов метода printFullName
-
-        printFullName("Иван", "Иванов");
-        printFullName("Мария", "Петрова");
-
-        // Вызов метода cornerValuesArray
-
-        String[] array = {"apple", "banana", "kiwi", "watermelon"};
-        String[] result = cornerValuesArray(array);
-        System.out.println("Shortest: " + result[0] + ", Longest: " + result[1]);
-
+        for (int num : numbers) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        return new int[]{min, max};
     }
 
+    // Метод для нахождения минимального и максимального символа в массиве char
+    public static char[] cornerValuesArray(char... chars) {
+        char min = chars[0];
+        char max = chars[0];
+
+        for (char c : chars) {
+            if (c < min) {
+                min = c;
+            }
+            if (c > max) {
+                max = c;
+            }
+        }
+        return new char[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"apple", "banana", "kiwi", "watermelon"};
+        String[] resultStr = cornerValuesArray(strings);
+        System.out.println("Shortest: " + resultStr[0] + ", Longest: " + resultStr[1]);
+
+        int[] numbers = {3, 5, 1, 8};
+        int[] resultInt = cornerValuesArray(numbers);
+        System.out.println("Min: " + resultInt[0] + ", Max: " + resultInt[1]);
+
+        char[] chars = {'d', 'a', 'c', 'b'};
+        char[] resultChar = cornerValuesArray(chars);
+        System.out.println("Min: " + resultChar[0] + ", Max: " + resultChar[1]);
+    }
 }
